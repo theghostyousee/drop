9bfe72a01f91b2bfa125ee533027dff9f4fe8790
theghostyousee <patrickserise174@gmail.com>
2023-04-24 21:12:34 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,4 @@
+function redirectTwitter() {
+    window.location.href = "https://twitter.com/intent/tweet?text=I%27m%20eligible%20for%20%40dropcoinarb%20airdrop%20%F0%9F%AA%82%0D%0A%0D%0A%24DROP%20is%20coming%20on%20%23Arbiturm%20&";
+  }
+  
